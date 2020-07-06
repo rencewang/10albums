eeade07fa2595ad337bbfe54ab125f637ac40b4c
law-wang <47406212+law-wang@users.noreply.github.com>
2020-07-06 12:27:04 -0700

    initial update

---
+++ src/components/year.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function PageYear(props) {
+    return <h1>{props.year}</h1>
+  }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import Link from "gatsby-link"
+import PageYear from "../components/year"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () =>
+  <div>
+    <h1>Hello world!</h1>
+    <PageYear year="2010" />
+    <Link to="/hi/">hi</Link>
+  </div>
