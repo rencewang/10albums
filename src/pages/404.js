c09fd1d4e7782cb5bff5a0b43ef3dfb0e5fdfcd6
law-wang <47406212+law-wang@users.noreply.github.com>
2020-07-12 11:03:41 -0700

    added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "../styles/album.sass"
+import "../styles/global.sass"
+
+export default () => {
+    return (
+        <main className="bg-404">
+            <div className="center-404">
+                <h2>10 Albums of the 2010s</h2>
+                <h4>Try a different page</h4>
+            </div>
+        </main>
+    )
+}
